0a4ce04fa55bda575e1083b6e08750aaad970061
wrobbinz <wrobbinz@gmail.com>
2018-02-04 14:21:57 -0800

    create login form

--- ui/src/js/loginForm.jsx
+++ ui/src/js/loginForm.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react'
 import axios from 'axios'
+import api from './api'
+
 
 class LoginForm extends Component {
   constructor(props) {
@@ -9,34 +12,54 @@ class LoginForm extends Component {
       email: '',
       password: '',
       username: '',
-
+      signup: false,
+      formColor: 'teal',
+      loginFailed: false,
     }
   }
 
-
-  async logIn(event) {
+  async logIn() {
     try {
-      const apiBaseUrl = 'http://localhost:3333/api/v1/'
       const payload = {
-        email: this.state.email.value,
-        password: this.state.password.value,
+        email: this.state.email,
+        password: this.state.password,
       }
-      const res = await axios.post(`${apiBaseUrl}login/`, payload)
+      console.log(payload)
+      const res = await axios.post(api.login, payload)
       window.sessionStorage.setItem('jwtToken', res.data.token)
+      this.props.loggedIn(true)
       console.log('Login successful', res)
     } catch (err) {
+      this.setState({
+        loginFailed: true,
+      })
       console.log(err)
     }
   }
-  async signUp(event) {
+
+  loadLogin() {
+    this.setState({
+      signup: false,
+      formColor: 'teal',
+    })
+  }
+  loadSignup() {
+    this.setState({
+      signup: true,
+      formColor: 'blue',
+      loginFailed: false,
+      password: '',
+    })
+  }
+
+  async signUp() {
     try {
-      const apiBaseUrl = 'http://localhost:3333/api/v1/'
       const payload = {
         email: this.state.email.value,
         username: this.state.username.value,
         password: this.state.password.value,
       }
-      await axios.post(`${apiBaseUrl}users/`, payload)
+      await axios.post(api.users, payload)
       this.logIn()
     } catch (err) {
       console.log(err)
@@ -45,23 +68,15 @@ class LoginForm extends Component {
 
   render() {
     return (
-      <div className="login-form">
-        <style>{`
-          body > div,
-          body > div > div,
-          body > div > div > div.login-form {
-            height: 100%;
-          }
-          `}
-        </style>
+      <div className="login-form full-height">
         <Grid
           textAlign="center"
           style={{ height: '100%' }}
           verticalAlign="middle"
         >
           <Grid.Column style={{ maxWidth: 450 }}>
-            <Header as="h2" color="teal" textAlign="center">
-              {' '}Log-in to your account
+            <Header as="h2" color={this.state.formColor} textAlign="center">
+              {' '}{ this.state.signup === true ? 'Sign Up' : 'Log In'}
             </Header>
             <Form size="large">
               <Segment>
@@ -70,40 +85,67 @@ class LoginForm extends Component {
                   icon="mail"
                   iconPosition="left"
                   placeholder="E-mail address"
-                  onChange={(event, newValue) => this.setState({ email: newValue })}
+                  value={this.state.email}
+                  onChange={event => this.setState({ email: event.target.value })}
                 />
-                <Form.Input
-                  fluid
-                  icon="user"
-                  iconPosition="left"
-                  placeholder="User Name"
-                  onChange={(event, newValue) => this.setState({ password: newValue })}
-                />
+                {this.state.signup ?
+                  <Form.Input
+                    fluid
+                    icon="user"
+                    iconPosition="left"
+                    placeholder="User Name"
+                    value={this.state.username}
+                    onChange={event => this.setState({ username: event.target.value })}
+                  /> : null }
                 <Form.Input
                   fluid
                   icon="lock"
                   iconPosition="left"
                   placeholder="Password"
                   type="password"
-                  onChange={(event, newValue) => this.setState({ password: newValue })}
+                  value={this.state.password}
+                  onChange={event => this.setState({ password: event.target.value })}
                 />
-
-                <Button onClick={event => this.logIn(event)} color="teal" fluid size="large">
-                  Login
-                </Button>
-                <Button onClick={event => this.signUp(event)} color="blue" fluid size="large">
-                  Sign Up
-                </Button>
+                { !this.state.signup ?
+                  <Button onClick={event => this.logIn(event)} color="teal" fluid size="large">
+                    Login
+                  </Button> :
+                  <Button onClick={event => this.signUp(event)} color="blue" fluid size="large">
+                    Sign Up
+                  </Button>
+                }
               </Segment>
             </Form>
-            <Message>
-              New to us? <a href="#">Sign Up</a>
-            </Message>
+            { this.state.loginFailed === true ?
+              <Message
+                error
+                header="Login Failed"
+                content="Email or password we're incorrect."
+              /> : null
+            }
+            { this.state.signup === true ?
+              <Message>
+                Already a user? <a href="#" onClick={() => this.loadLogin()}>Log In</a>
+              </Message> :
+              <Message>
+                New to us? <a href="#" onClick={() => this.loadSignup()}>Sign Up</a>
+              </Message>
+            }
+
+
           </Grid.Column>
         </Grid>
       </div>
     )
   }
+}
+
+LoginForm.propTypes = {
+  loggedIn: PropTypes.func,
+}
+
+LoginForm.defaultProps = {
+  loggedIn: false,
 }
 
 export default LoginForm
