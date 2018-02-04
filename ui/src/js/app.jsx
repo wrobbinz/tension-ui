5b5508573ec7306e0cb6608fdf25b7cbc8d32b5a
wrobbinz <wrobbinz@gmail.com>
2018-02-04 14:22:24 -0800

    show login vs workspace

--- ui/src/js/app.jsx
+++ ui/src/js/app.jsx
@@ -2,21 +2,36 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import '../../semantic/dist/semantic.min.css'
+import '../css/style.css'
 import LoginForm from './loginForm'
 import Workspace from './workspace'
 
-import '../css/style.css'
-
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loggedIn: false,
+    }
+    this.setLoginState = this.setLoginState.bind(this)
+  }
+
+  setLoginState(value) {
+    this.setState({
+      loggedIn: value,
+    })
+  }
+
   render() {
-    if (!window.sessionStorage.getItem('jwtToken')) {
+    if (this.state.loggedIn === true || window.sessionStorage.getItem('jwtToken')) {
       return (
-        <LoginForm />
+        <Workspace className="full-height" />
       )
     } else {
       return (
-        <Workspace />
+        <LoginForm
+          loggedIn={this.setLoginState}
+        />
       )
     }
   }
