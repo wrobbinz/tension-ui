b44af3db33c9b3aaffda82652661526d86015f44
wrobbinz <wrobbinz@gmail.com>
2018-02-04 14:21:32 -0800

    create api module

---
+++ ui/src/js/api.js
@@ -0,0 +1,14 @@
+const base = 'http://localhost:3333/api/v1/'
+const api = {
+  notes: `${base}notes/`,
+  users: `${base}users/`,
+  login: `${base}login/`,
+
+  config: {
+    headers: {
+      Authorization: `Bearer ${window.sessionStorage.getItem('jwtToken')}`,
+    },
+  },
+}
+
+export default api
