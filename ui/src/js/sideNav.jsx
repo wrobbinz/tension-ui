74ae572fcac182c6b8d5c72409681d772c43d93a
wrobbinz <wrobbinz@gmail.com>
2018-02-08 23:11:07 -0800

    extract note logic out of sidenav

--- ui/src/js/sideNav.jsx
+++ ui/src/js/sideNav.jsx
@@ -1,121 +1,19 @@
 import React, { Component } from 'react'
-import { Sidebar, Segment, Menu, Icon, Input } from 'semantic-ui-react'
-import axios from 'axios'
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
-import { remove, reverse } from 'lodash'
-import api from './api'
+import { Sidebar, Segment, Menu, Icon, Popup } from 'semantic-ui-react'
 import '../css/style.css'
-import Editor from './editor'
-import NoteOptions from './noteOptions'
+import WorkMenu from './workMenu'
 
-
-// a little function to help us with reordering the result
-const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list)
-  const [removed] = result.splice(startIndex, 1)
-  result.splice(endIndex, 0, removed)
-
-  return result
-}
 
 class SideNav extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      visible: true,
-      notes: [],
-      note: {
-        title: '...',
-        contents: '',
-      },
+      focus: 'notes',
     }
-    this.selectNote = this.selectNote.bind(this)
-    this.createNote = this.createNote.bind(this)
-    this.deleteNote = this.deleteNote.bind(this)
-    this.toggleVisibility = this.toggleVisibility.bind(this)
-    this.updateContents = this.updateContents.bind(this)
-    this.updateTitle = this.updateTitle.bind(this)
-    this.onDragEnd = this.onDragEnd.bind(this)
-
-    axios.get(api.notes, api.config())
-      .then((res) => {
-        const notes = reverse(res.data)
-        const note = notes[0]
-        this.setState({ notes, note })
-        this.setState({ notes })
-      })
   }
 
-  onDragEnd(result) {
-    // dropped outside the list
-    if (!result.destination) {
-      return
-    }
-
-    const notes = reorder(
-      this.state.notes,
-      result.source.index,
-      result.destination.index,
-    )
-
-    this.setState({
-      notes,
-    })
-  }
-
-  toggleVisibility() {
-    this.setState({
-      visible: !this.state.visible,
-
-    })
-  }
-
-  selectNote(id) {
-    const note = this.state.notes.find(n => n.id === id)
-    console.log('selected', note)
-    this.setState({ note })
-  }
-
-  createNote() {
-    const payload = {
-      title: '',
-      contents: '',
-    }
-    axios.post(api.notes, payload, api.config())
-      .then((res) => {
-        const { notes } = this.state
-        notes.unshift(res.data)
-        const note = res.data
-        this.setState({ notes, note })
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-
-  deleteNote(id) {
-    axios.delete(`${api.notes}${id}`, api.config())
-      .then(() => {
-        const { notes } = this.state
-        remove(notes, note => note.id === id)
-        const note = notes[0] || null
-        this.setState({ notes, note })
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-
-  updateTitle(event) {
-    const { note } = this.state
-    note.title = event.target.value
-    this.setState({ note })
-  }
-
-  updateContents(event) {
-    const { note } = this.state
-    note.contents = event.target.value
-    this.setState({ note })
+  handleClick(focus) {
+    this.setState({ focus })
   }
 
   render() {
@@ -124,72 +22,62 @@ class SideNav extends Component {
         <Sidebar.Pushable as={Segment} className="no-border-radius no-border">
           <Sidebar
             as={Menu}
-            width="thin"
+            width="very thin"
             animation="push"
-            visible={this.state.visible}
-            className="full-height"
+            visible
+            className="full-height sidenav"
+            icon
             vertical
             inverted
           >
-            <Menu.Item>Notes
-              <Icon link onClick={() => { this.createNote() }} name="plus" />
-            </Menu.Item>
-            <Menu.Menu>
-              <DragDropContext onDragEnd={this.onDragEnd}>
-                <Droppable droppableId="droppable">
-                  {provided => (
-                    <div
-                      ref={provided.innerRef}
-                    >
-                      {this.state.notes.map((note, index) => (
-                        <Draggable key={note.id} draggableId={note.id} index={index}>
-                          {prov => (
-                            <div>
-                              <div
-                                ref={prov.innerRef}
-                                {...prov.draggableProps}
-                                {...prov.dragHandleProps}
-                              >
-                                <Menu.Item
-                                  onClick={() => { this.selectNote(note.id) }}
-                                  name={note.id.toString()}
-                                  active={this.state.note.id === note.id}
-                                  id={note.id}
-                                >
-                                  {note.title === '' ? 'Untitled Note' : note.title}
-                                  <NoteOptions
-                                    note={this.state.note}
-                                    deleteNote={this.deleteNote}
-                                  />
-                                </Menu.Item>
-                              </div>
-                              {prov.placeholder}
-                            </div>
-                          )}
-                        </Draggable>
-                      ))}
-                    </div>
-                  )}
-                </Droppable>
-              </DragDropContext>
-            </Menu.Menu>
+            <Popup
+              trigger={
+                <Menu.Item
+                  onClick={() => { this.handleClick('notes') }}
+                  active={this.state.focus === 'notes'}
+                >
+                  <Icon link size="large" name="sticky note" />
+                </Menu.Item>
+              }
+              mouseEnterDelay={2000}
+              content="Notes"
+              position="right center"
+              size="mini"
+              inverted
+            />
+            <Popup
+              trigger={
+                <Menu.Item
+                  onClick={() => { this.handleClick('tasks') }}
+                  active={this.state.focus === 'tasks'}
+                >
+                  <Icon link size="large" name="tasks" />
+                </Menu.Item>
+              }
+              mouseEnterDelay={2000}
+              content="Tasks"
+              position="right center"
+              size="mini"
+              inverted
+            />
+            <Popup
+              trigger={
+                <Menu.Item
+                  onClick={() => { this.handleClick('sketches') }}
+                  active={this.state.focus === 'sketches'}
+                >
+                  <Icon link size="large" name="paint brush" />
+                </Menu.Item>
+              }
+              mouseEnterDelay={2000}
+              content="Sketches"
+              position="right center"
+              size="mini"
+              inverted
+            />
           </Sidebar>
           <Sidebar.Pusher className="full-height">
-            {this.state.note ?
-              <div>
-                <Icon link onClick={this.toggleVisibility} flipped={this.state.visible ? 'horizontally' : null} name="angle right" size="big" />
-                <Input
-                  id="titleInput"
-                  className="no-border"
-                  size="big"
-                  placeholder="Title"
-                  value={this.state.note ? this.state.note.title : ''}
-                  maxLength="20"
-                  onChange={(event) => { this.updateTitle(event) }}
-                />
-                <Editor updateContents={this.updateContents} note={this.state.note} />
-              </div> : null
-            }
+            <WorkMenu focus={this.state.focus} />
           </Sidebar.Pusher>
         </Sidebar.Pushable>
       </div>
