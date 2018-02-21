e4d8530554c13da53f730cfb3412d61d15f5e745
Will Robbins <will@sighten.io>
2018-02-21 00:30:36 -0700

    create settings component (modal)

---
+++ ui/src/js/settings.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { List, Icon, Modal, Header, Button } from 'semantic-ui-react'
+
+class Settings extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+  render() {
+    return (
+      <Modal
+        closeOnDimmerClick={false}
+        trigger={
+          <List.Item
+            icon="options"
+            content="Settings"
+            className="pointer"
+          />
+        }
+        basic
+        size="small"
+      >
+        <Header
+          icon="settings"
+          content="User Settings"
+        />
+        <Modal.Content>
+          <p>Are you sure you want to delete this note? (This action cannot be undone)</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button basic color="grey" inverted>
+            <Icon name="remove" /> Cancel
+          </Button>
+          <Button color="green" inverted>
+            <Icon name="check" /> Save
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    )
+  }
+}
+
+export default Settings
