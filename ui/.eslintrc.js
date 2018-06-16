5995015105a4e65371e428d2cde4070c375ed439
wrobbinz <wrobbinz@gmail.com>
2018-06-15 23:21:58 -0700

    remove link lint error

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     'rules': {
         'semi': [2, 'never'],
         'react/jsx-filename-extension': [0, 'off'],
+        'jsx-a11y/anchor-is-valid': [0, 'off'],
     },
     'env': {
         'browser': true,
